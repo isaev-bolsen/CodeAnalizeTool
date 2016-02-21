eff3d1cb9d51b3dade156b60dc28583b65629fbf
isaev-bolsen <al_mamistov@mail.ru>
2016-02-21 21:24:43 +0300

    It works (more tests added)

---
+++ AnalizerTests/GetHierarhyTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace AnalizerTests
+{
+    [TestClass]
+    public class GetHierarhyTests
+    {
+        [TestMethod]
+        public void GetHierarhySimple()
+        {
+            Analizer.MyTypeInfo res = new Analizer.Analizer(this.GetType().Assembly).GetHierarhy("AnalizerTests.SimpleHierarhyRoot");
+            Assert.AreSame(typeof(SimpleHierarhyRoot), res.Type);
+            Assert.AreSame(typeof(SimpleHierarhyChild), res.Children.Single().Type);
+        }
+
+        [TestMethod]
+        public void GetHierarhyMoreComplex()
+        {
+            Analizer.MyTypeInfo res = new Analizer.Analizer(this.GetType().Assembly).GetHierarhy("AnalizerTests.MoreComplexHierarhyRoot");
+            var children = res.Children.ToArray();
+            Assert.AreEqual(2, children.Length);
+            Assert.AreEqual(2, children[0].Children.Count());
+            Assert.AreEqual(2, children[1].Children.Count());
+        }
+
+        [TestMethod]
+        public void GetHierarhyMoreComplexFromChild()
+        {
+            Analizer.MyTypeInfo res = new Analizer.Analizer(this.GetType().Assembly).GetHierarhy("AnalizerTests.MoreComplexHierarhyChild22");
+            var children = res.Children.ToArray();
+            Assert.AreEqual(2, children.Length);
+            Assert.AreEqual(2, children[0].Children.Count());
+            Assert.AreEqual(2, children[1].Children.Count());
+        }
+    }
+}
---
+++ AnalizerTests/MoreComplexHierarhy.cs
@@ -0,0 +1,30 @@
+﻿namespace AnalizerTests
+{
+    class MoreComplexHierarhyRoot
+    {
+    }
+
+    class MoreComplexHierarhyChild1 : MoreComplexHierarhyRoot
+    {
+    }
+
+    class MoreComplexHierarhyChild2 : MoreComplexHierarhyRoot
+    {
+    }
+
+    class MoreComplexHierarhyChild11 : MoreComplexHierarhyChild1
+    {
+    }
+
+    class MoreComplexHierarhyChild12 : MoreComplexHierarhyChild1
+    {
+    }
+
+    class MoreComplexHierarhyChild21 : MoreComplexHierarhyChild2
+    {
+    }
+
+    class MoreComplexHierarhyChild22 : MoreComplexHierarhyChild2
+    {
+    }
+}
