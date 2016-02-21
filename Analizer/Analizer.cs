7c35e89c4992917d325fa0370ca99d2ffd20bddc
isaev-bolsen <al_mamistov@mail.ru>
2016-02-21 10:50:35 +0300

    Mk1. Searching for hierarhy root

---
+++ Analizer/Analizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Analizer
+{
+    public class Analizer
+    {
+        private IEnumerable<Assembly> assembleys;
+
+        public Analizer(IEnumerable<Assembly> assembleys)
+        {
+            this.assembleys = assembleys;
+        }
+
+        public Analizer(Assembly assembley) : this(new Assembly[] { assembley }) { }
+
+        public Type GetRootForHierarhy(string ClassName)
+        {
+            return GetRootForHierarhy(assembleys.Select(a => a.GetType(ClassName)).Single());
+        }
+
+        public Type GetRootForHierarhy(Type type)
+        {
+            if (type == typeof(object) || type.BaseType == typeof(object)) return type;
+            else return GetRootForHierarhy(type.BaseType);
+        }
+    }
+}
---
+++ AnalizerTests/RootTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnalizerTests
+{
+    [TestClass]
+    public class RootTests
+    {
+        [TestMethod]
+        public void SimpleRootNormalByType()
+        {
+            Assert.AreSame(typeof(SimpleHierarhyRoot), new Analizer.Analizer(typeof(SimpleHierarhyChild).Assembly).GetRootForHierarhy(typeof(SimpleHierarhyChild)));
+        }
+
+        [TestMethod]
+        public void SimpleRootNormalByTypeName()
+        {
+            Assert.AreSame(typeof(SimpleHierarhyRoot), new Analizer.Analizer(typeof(SimpleHierarhyChild).Assembly).GetRootForHierarhy("AnalizerTests.SimpleHierarhyChild"));
+        }
+    }
+}
