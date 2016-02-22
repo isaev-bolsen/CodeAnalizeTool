2524213b75a29b1ac1642c149c8a85915aa70443
isaev-bolsen <al_mamistov@mail.ru>
2016-02-22 16:51:40 +0300

    Drawing test

---
+++ AnalizerTests/Drawing.cs
@@ -0,0 +1,29 @@
+﻿using HierarhyDrawer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace AnalizerTests
+{
+    /// <summary>
+    /// Сводное описание для Drawing
+    /// </summary>
+    [TestClass]
+    public class Drawing
+    {
+        Canvas canvas = new Canvas();
+        Drawer drawer;
+
+        public Drawing()
+        {
+            drawer = new Drawer(canvas);
+        }
+
+        [TestMethod]
+        public void DrawSimpleHierarhy()
+        {
+            drawer.Draw(new Analizer.Analizer(this.GetType().Assembly).GetHierarhy("AnalizerTests.SimpleHierarhyRoot"));
+            Assert.AreEqual(2, canvas.Children.OfType<TextBox>().Count());
+        }
+    }
+}
--- HierarhyDrawer/Drawer.cs
+++ HierarhyDrawer/Drawer.cs
@@ -6,7 +6,7 @@ using System.Windows.Controls;
 
 namespace HierarhyDrawer
 {
-    internal class Drawer
+    public class Drawer
     {
         private Canvas Canvas;
 
