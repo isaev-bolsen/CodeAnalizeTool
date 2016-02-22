f5cb9c2c9dc0430d30abe69d6dbf9aadf2c0a589
isaev-bolsen <al_mamistov@mail.ru>
2016-02-22 21:00:15 +0300

    The most crazy  abstraction ever

--- HierarhyDrawer/Drawer.cs
+++ HierarhyDrawer/Drawer.cs
@@ -1,6 +1,4 @@
 ﻿using Analizer;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -8,34 +6,21 @@ namespace HierarhyDrawer
 {
     public class Drawer
     {
-        private Canvas canvas;
-
+        private TreeDrawer.TreeDrawer<MyTypeInfo> treeDrawer;
         public Drawer(Canvas canvas)
         {
-            this.canvas = canvas;
-            this.canvas.Children.Clear();
+            treeDrawer = new TreeDrawer.TreeDrawer<MyTypeInfo>(canvas,TI=>TI.Children, GetNodeRepresentation);
         }
 
-        protected virtual UIElement GetNodeRepresentation(MyTypeInfo typeInfo)
+        private static UIElement GetNodeRepresentation(MyTypeInfo typeInfo)
         {
             return new TextBox() { Text = typeInfo.Type.FullName, IsReadOnly = true, Height = 25 };
         }
 
         public void Draw(MyTypeInfo root)
         {
-            UIElement UIroot = GetNodeRepresentation(root);
-            canvas.Children.Add(UIroot);
-            Draw(UIroot, root.Children);
-        }
-
-        private void Draw(UIElement Parent, IEnumerable<MyTypeInfo> Children)
-        {
-            IEnumerable<UIElement> ChildElemnts = Children.Select(N => GetNodeRepresentation(N));
-            foreach (UIElement elt in ChildElemnts)
-            {
-                canvas.Children.Add(elt);
-                Canvas.SetTop(elt, 30);
-            }
+            treeDrawer.Clear();
+            treeDrawer.Draw(root);
         }
     }
 }
---
+++ TreeDrawer/TreeDrawer.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TreeDrawer
+{
+    public class TreeDrawer<T>
+    {
+        private class Node
+        {
+            public T element;
+            public Node Parent;
+            public List<Node> Children = new List<Node>();
+            public int level = 0;
+            public double XPosition = 0;
+            public double YPosition = 0;
+            public UIElement payload;
+        }
+
+        List<List<Node>> Levels = new List<List<Node>>();
+
+        private Canvas canvas;
+
+        public delegate IEnumerable<T> GetChildren(T elment);
+        public delegate UIElement GetPayLoad(T element);
+
+        private GetChildren getChildren;
+        private GetPayLoad getPayLoad;
+
+        public TreeDrawer(Canvas canvas, GetChildren GetChildrenMethod, GetPayLoad GetPayLoadMethod)
+        {
+            this.canvas = canvas;
+            getChildren = GetChildrenMethod;
+            getPayLoad = GetPayLoadMethod;
+        }
+
+        public void Clear()
+        {
+            canvas.Children.Clear();
+            Levels.Clear();
+        }
+
+        private IEnumerable<Node> CreateChildNodes(Node parent, int level)
+        {
+            return getChildren(parent.element).Select(e =>
+            {
+                var childnode = new Node { element = e, Parent = parent, level = level, payload = getPayLoad(e) };
+                parent.Children.Add(childnode);
+                return childnode;
+            }).ToList();
+        }
+
+        private List<Node> GetLevel(int PreviousLevel)
+        {
+            return Levels[PreviousLevel].SelectMany(n => CreateChildNodes(n, PreviousLevel + 1)).ToList();
+        }
+
+        public void Draw(T root)
+        {
+            int currentLevel = 0;
+            Clear();
+            Node RootNode = new Node() { element = root, payload = getPayLoad(root) };
+            Levels.Add(new List<Node> { RootNode });
+            do
+            {
+                Levels.Add(GetLevel(currentLevel));
+                currentLevel++;
+            } while (Levels[currentLevel].Count > 0);
+
+            foreach (var level in Levels)
+                foreach (var element in level) canvas.Children.Add(element.payload);
+        }
+    }
+}
